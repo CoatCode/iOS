2b2fb8cd4199a957f1722670f1bab98a18aca326
KangMinseok <qbq5000@gmail.com>
2020-06-28 18:01:36 +0900

    Initial Commit

---
+++ CoatCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/06/28.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

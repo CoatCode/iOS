ae4833774972a25dc1d486271f909cf49ba89e17
KangMinseok <qbq5000@gmail.com>
2020-08-10 23:54:47 +0900

    Coordinator 구조 구성

--- CoatCode/Application/AppCoordinator.swift
+++ CoatCode/Application/AppCoordinator.swift
@@ -6,5 +6,34 @@
 //  Copyright © 2020 MinseokKang. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import RxSwift
 
+class AppCoordinator: BaseCoordinator {
+    
+    static let shared = AppCoordinator()
+    
+    private override init() {}
+    
+    var window = UIWindow(frame: UIScreen.main.bounds)
+    
+    override func start() {
+        self.navigationController.navigationBar.isHidden = true
+        self.window.rootViewController = self.navigationController
+        self.window.makeKeyAndVisible()
+        
+        if loggedIn.value {
+            print("Go To Main")
+        } else {
+            let coordinator = IntroCoordinator()
+            coordinator.navigationController = self.navigationController
+            self.start(coordinator: coordinator)
+        }
+    }
+    
+    func didSignIn() {
+//        let coordinator = MainCoordinator()
+//        coordinator.navigationController = self.navigationController
+//        self.start(coordinator: coordinator)
+    }
+}

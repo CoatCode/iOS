ee3cb8b1b6d42443930a42c28fa6022c45158bcd
KangMinseok <qbq5000@gmail.com>
2020-08-22 21:08:01 +0900

    AppFlow 코드 정리 & AppStepper 콜백메서드 수정

--- CoatCode/Flows/AppFlow.swift
+++ CoatCode/Flows/AppFlow.swift
@@ -13,6 +13,7 @@ import RxSwift
 
 class AppFlow: Flow {
     
+    // MARK: - Properties
     private let services: CoatCodeService
     
     var root: Presentable {
@@ -25,6 +26,7 @@ class AppFlow: Flow {
         return viewController
     }()
     
+    // MARK: - Init
     init(services: CoatCodeService) {
         self.services = services
     }
@@ -33,31 +35,37 @@ class AppFlow: Flow {
         print("\(type(of: self)): \(#function)")
     }
     
+    // MARK: - Navigation Switch
     func navigate(to step: Step) -> FlowContributors {
         guard let step = step as? CoatCodeStep else { return .none }
         
         switch step {
         case .introIsRequired:
             return navigateToIntro()
-//        case .tabBarIsRequired:
-//            return navigationToTabBar()
+            //        case .tabBarIsRequired:
+        //            return navigationToTabBar()
         default:
             return .none
         }
     }
-    
+}
+
+// MARK: - Navigate to TabBar
+//extension AppFlow {
 //    private func navigationToTabBar() -> FlowContributors {
 //        let tabBarFlow = TabBarFlow(withServices: self.services)
 //
 //        Flows.use(tabBarFlow, when: .created) { [unowned self] root in
 //            self.rootViewController = root
-////            self.rootViewController.pushViewController(root, animated: false)
 //        }
 //
 //        return .one(flowContributor: .contribute(withNextPresentable: tabBarFlow,
 //                                                 withNextStepper: OneStepper(withSingleStep: CoatCodeStep.tabBarIsRequired)))
 //    }
-    
+//}
+
+// MARK: - Navigate to Intro
+extension AppFlow {
     private func navigateToIntro() -> FlowContributors {
         let introFlow = IntroFlow(withServices: self.services)
         
@@ -68,21 +76,21 @@ class AppFlow: Flow {
         return .one(flowContributor: .contribute(withNextPresentable: introFlow,
                                                  withNextStepper: OneStepper(withSingleStep: CoatCodeStep.introIsRequired)))
     }
-    
-    
 }
 
 // 앱 시작시 로그인 유무를 확인하여 TabBar화면을 표시할지, Intro화면을 표시할지를 정한다.
 // 로그인 성공시, TabBar화면을 표시해야함.
 class AppStepper: Stepper {
     
+    let disposeBag = DisposeBag()
+    
     let steps = PublishRelay<Step>()
     private let appServices: CoatCodeService
-
+    
     init(withServices services: CoatCodeService) {
         self.appServices = services
     }
-
+    
     // 로그인 여부를 확인하여 Intro화면을 보여줄지 TabBar화면(main)을 줄지를 결정한다.
     var initialStep: Step {
         if loggedIn.value {
@@ -91,13 +99,15 @@ class AppStepper: Stepper {
             return CoatCodeStep.introIsRequired
         }
     }
-
+    
+    // 로그인 성공시 실행되는 콜백 메서드
     /// callback used to emit steps once the FlowCoordinator is ready to listen to them to contribute to the Flow
-//    func readyToEmitSteps() {
-//        if loggedIn.value {
-//            steps.accept(CoatCodeStep.tabBarIsRequired)
-//        } else {
-//            steps.accept(CoatCodeStep.introIsRequired)
-//        }
-//    }
+    func readyToEmitSteps() {
+        loggedIn
+            .subscribe(onNext: { [weak self] isLogin in
+                if isLogin {
+                    self?.steps.accept(CoatCodeStep.tabBarIsRequired)
+                }
+            }).disposed(by: disposeBag)
+    }
 }

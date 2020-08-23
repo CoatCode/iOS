edd914a74fb3e5d6e02d89a50a7f773f71593d89
KangMinseok <qbq5000@gmail.com>
2020-08-23 21:03:37 +0900

    extension 추가 + String + UIImagePicker

---
+++ CoatCode/Extensions/RxSwift/UIImagePickerController+Rx.swift
@@ -0,0 +1,47 @@
+//
+//  UIImagePickerController+Rx.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/08/23.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+#if os(iOS)
+    
+    import RxSwift
+    import RxCocoa
+    import UIKit
+
+    extension Reactive where Base: UIImagePickerController {
+
+        /**
+         Reactive wrapper for `delegate` message.
+         */
+        public var didFinishPickingMediaWithInfo: Observable<[String : AnyObject]> {
+            return delegate
+                .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
+                .map({ (a) in
+                    return try castOrThrow(Dictionary<String, AnyObject>.self, a[1])
+                })
+        }
+
+        /**
+         Reactive wrapper for `delegate` message.
+         */
+        public var didCancel: Observable<()> {
+            return delegate
+                .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerControllerDidCancel(_:)))
+                .map {_ in () }
+        }
+        
+    }
+    
+#endif
+
+private func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
+    guard let returnValue = object as? T else {
+        throw RxCocoaError.castingError(object: object, targetType: resultType)
+    }
+
+    return returnValue
+}

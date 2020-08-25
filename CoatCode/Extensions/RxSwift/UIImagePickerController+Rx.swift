6bb1d5e58a29752da33b53862f1cf43e5176568f
KangMinseok <qbq5000@gmail.com>
2020-08-25 21:14:19 +0900

    extension 수정 + imagePicker

--- CoatCode/Extensions/RxSwift/UIImagePickerController+Rx.swift
+++ CoatCode/Extensions/RxSwift/UIImagePickerController+Rx.swift
@@ -6,42 +6,58 @@
 //  Copyright © 2020 MinseokKang. All rights reserved.
 //
 
-#if os(iOS)
-    
-    import RxSwift
-    import RxCocoa
-    import UIKit
+import RxSwift
+import RxCocoa
+import UIKit
 
-    extension Reactive where Base: UIImagePickerController {
+extension Reactive where Base: UIImagePickerController {
 
-        /**
-         Reactive wrapper for `delegate` message.
-         */
-        public var didFinishPickingMediaWithInfo: Observable<[String : AnyObject]> {
-            return delegate
-                .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
-                .map({ (a) in
-                    return try castOrThrow(Dictionary<String, AnyObject>.self, a[1])
-                })
-        }
-
-        /**
-         Reactive wrapper for `delegate` message.
-         */
-        public var didCancel: Observable<()> {
-            return delegate
-                .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerControllerDidCancel(_:)))
-                .map {_ in () }
-        }
-        
+    var didFinishPickingMediaWithInfo: Observable<[UIImagePickerController.InfoKey: AnyObject]> {
+        return RxImagePickerDelegateProxy.proxy(for: base)
+            .methodInvoked(#selector(UIImagePickerControllerDelegate.imagePickerController(_:didFinishPickingMediaWithInfo:)))
+            .map({ (a) in
+                return try castOrThrow(Dictionary<UIImagePickerController.InfoKey, AnyObject>.self, a[1])
+            })
     }
-    
-#endif
+}
 
-private func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
+func castOrThrow<T>(_ resultType: T.Type, _ object: Any) throws -> T {
     guard let returnValue = object as? T else {
         throw RxCocoaError.castingError(object: object, targetType: resultType)
     }
 
     return returnValue
+}
+
+func imagePickerScene(on presenter: UIViewController, modalPresentationStyle: UIModalPresentationStyle? = nil, modalTransitionStyle: UIModalTransitionStyle? = nil) -> (_ observer: AnyObserver<[UIImagePickerController.InfoKey: AnyObject]>) -> Disposable {
+    return { [weak presenter] observer in
+        let controller = UIImagePickerController()
+        if let presentationStyle = modalPresentationStyle {
+            controller.modalPresentationStyle = presentationStyle
+        }
+        if let transitionStyle = modalTransitionStyle {
+            controller.modalTransitionStyle = transitionStyle
+        }
+        presenter?.present(controller, animated: true)
+        return controller.rx.didFinishPickingMediaWithInfo
+            .do(onNext: { _ in
+                presenter?.dismiss(animated: true)
+            })
+            .subscribe(observer)
+    }
+}
+
+final class RxImagePickerDelegateProxy: DelegateProxy<UIImagePickerController, UINavigationControllerDelegate & UIImagePickerControllerDelegate>, DelegateProxyType, UINavigationControllerDelegate & UIImagePickerControllerDelegate {
+
+    static func currentDelegate(for object: UIImagePickerController) -> (UIImagePickerControllerDelegate & UINavigationControllerDelegate)? {
+        return object.delegate
+    }
+
+    static func setCurrentDelegate(_ delegate: (UIImagePickerControllerDelegate & UINavigationControllerDelegate)?, to object: UIImagePickerController) {
+        object.delegate = delegate
+    }
+
+    static func registerKnownImplementations() {
+        self.register { RxImagePickerDelegateProxy(parentObject: $0, delegateProxy: RxImagePickerDelegateProxy.self) }
+     }
 }

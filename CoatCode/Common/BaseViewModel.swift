56c53f2016a50c983e0a60cd53346da7b244d90f
KangMinseok <qbq5000@gmail.com>
2020-08-31 21:15:13 +0900

    [Modify] Base ViewModel 수정

---
+++ CoatCode/Common/BaseViewModel.swift
@@ -0,0 +1,34 @@
+//
+//  ViewModel.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/08/30.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class BaseViewModel: ServicesViewModel, Stepper {
+    
+    // MARK: - Properties
+    let disposeBag = DisposeBag()
+    
+    let steps = PublishRelay<Step>()
+    var services: CoatCodeService!
+    
+    let loading = ActivityIndicator()
+    let error = PublishSubject<ResponseError>()
+    
+    // MARK: - Dummy
+    struct Input {
+    }
+    
+    struct Output {
+    }
+    
+    func transform(input: Input) -> Output {
+        return Output()
+    }
+}

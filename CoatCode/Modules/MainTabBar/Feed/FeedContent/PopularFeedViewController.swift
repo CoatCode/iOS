59404c28a1af0b204a654aeb0d07113f0d4c8446
KangMinseok <qbq5000@gmail.com>
2020-09-04 20:59:09 +0900

    [Add] 피드 게시물 뷰 추가

---
+++ CoatCode/Modules/MainTabBar/Feed/FeedContent/PopularFeedViewController.swift
@@ -0,0 +1,35 @@
+//
+//  PopularFeedViewController.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/04.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import XLPagerTabStrip
+
+class PopularFeedViewController: UIViewController, IndicatorInfoProvider {
+    
+    func indicatorInfo(for pagerTabStripController: PagerTabStripViewController) -> IndicatorInfo {
+        return "인기"
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

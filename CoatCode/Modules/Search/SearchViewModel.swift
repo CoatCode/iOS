bda8041ac9eb7793a19da0b4684262ba6201a60f
KangMinseok <qbq5000@gmail.com>
2020-09-06 17:03:49 +0900

    [Add] SearchViewModel 추가

---
+++ CoatCode/Modules/Search/SearchViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  SearchViewModel.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/06.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class SearchViewModel: BaseViewModel {
+    
+    
+    
+}

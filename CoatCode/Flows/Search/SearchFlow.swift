de4bf393d442c9c80778b65f305023a0e3da1aac
KangMinseok <qbq5000@gmail.com>
2020-09-06 20:53:07 +0900

    [Modify] 검색 Flow 수정

--- CoatCode/Flows/Search/SearchFlow.swift
+++ CoatCode/Flows/Search/SearchFlow.swift
@@ -10,6 +10,46 @@ import RxFlow
 import RxSwift
 import RxCocoa
 
-class SearchFlow {
+class SearchFlow: Flow {
     
+    // MARK: - Properties
+    private let services: CoatCodeService
+    
+    var root: Presentable {
+        return self.rootViewController
+    }
+    
+    var rootViewController = UIViewController()
+    
+    // MARK: - Init
+    init(withServices services: CoatCodeService) {
+        self.services = services
+    }
+    
+    deinit {
+        print("\(type(of: self)): \(#function)")
+    }
+    
+    func navigate(to step: Step) -> FlowContributors {
+        guard let step = step as? CoatCodeStep else { return .none }
+        
+        switch step {
+        case .searchIsRequired:
+            return navigateToSearch()
+        default:
+            return .none
+        }
+    }
+}
+
+extension SearchFlow {
+    func navigateToSearch() -> FlowContributors {
+        let viewModel = SearchViewModel()
+        let viewController = SearchViewController.instantiate(withViewModel: viewModel,
+                                                              andServices: self.services)
+                
+        self.rootViewController.addChild(viewController)
+        return .one(flowContributor: .contribute(withNextPresentable: viewController,
+                                                 withNextStepper: viewModel))
+    }
 }

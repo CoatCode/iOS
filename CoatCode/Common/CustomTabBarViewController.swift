2d205dfeed88c7fdbc5af56720e7b96ddf4a282c
KangMinseok <qbq5000@gmail.com>
2020-09-06 20:52:24 +0900

    [Modify] CustomTabBarViewController 수정 + NavigationBar 커스텀

--- CoatCode/Common/CustomTabBarViewController.swift
+++ CoatCode/Common/CustomTabBarViewController.swift
@@ -24,7 +24,7 @@ class CustomTabBarViewController: ButtonBarPagerTabStripViewController, ViewMode
     
     // MARK: - View life cycle
     override public func viewDidLoad() {
-        setCustomTabBar()
+        configureCustomTabBar()
         super.viewDidLoad()
         
         bindViewModel()
@@ -53,12 +53,14 @@ class CustomTabBarViewController: ButtonBarPagerTabStripViewController, ViewMode
         }).disposed(by: disposeBag)
     }
     
-    // MARK: - SetCustomTabBar
-    public func setCustomTabBar() {
+    // MARK: - Configure CustomTabBar
+    public func configureCustomTabBar() {
         // change selected bar color
-        settings.style.buttonBarBackgroundColor = .white
-        settings.style.buttonBarItemBackgroundColor = .white
-        settings.style.selectedBarBackgroundColor = UIColor(red: 0.20, green: 0.20, blue: 0.20, alpha: 1.00)
+        settings.style.buttonBarBackgroundColor = Configs.BaseColor.navBarTintColor
+        settings.style.buttonBarItemBackgroundColor = Configs.BaseColor.navBarTintColor
+        
+        settings.style.selectedBarBackgroundColor = Configs.BaseColor.signatureColor
+        
         settings.style.buttonBarItemFont = .boldSystemFont(ofSize: 14)
         settings.style.selectedBarHeight = 1.5
         settings.style.buttonBarMinimumLineSpacing = 0
@@ -69,12 +71,25 @@ class CustomTabBarViewController: ButtonBarPagerTabStripViewController, ViewMode
         changeCurrentIndexProgressive = { (oldCell: ButtonBarViewCell?, newCell: ButtonBarViewCell?, progressPercentage: CGFloat, changeCurrentIndex: Bool, animated: Bool) -> Void in
             guard changeCurrentIndex == true else { return }
             oldCell?.label.textColor = .lightGray
-            newCell?.label.textColor = UIColor(red: 0.20, green: 0.20, blue: 0.20, alpha: 1.00)
+            newCell?.label.textColor = Configs.BaseColor.signatureColor
         }
     }
     
-    // MARK: - ProfileShortcutBarButton
-    func profileShortcutBarButton() {
+    // MARK: - Configure Navigation Bar
+    func configureBaseNaviBar() {
+        self.navigationController?.navigationBar.barTintColor = Configs.BaseColor.navBarTintColor
+        self.navigationController?.navigationBar.shadowImage = UIImage()
+        
+        setIconBar() // Left
+        setProfileShortcutBar() // Title
+        setSearchBar() // Right
+    }
+    
+    func setIconBar() {
+        self.navigationItem.leftBarButtonItem = UIBarButtonItem(image: UIImage(named: "cocoIcon"), style: .plain, target: nil, action: nil)
+    }
+    
+    func setProfileShortcutBar() {
         let profileBarButton = ImageBarButton(withImage: UIImage(named: "Default_Profile"))
         profileBarButton.button.addTarget(self, action: #selector(requestProfileView), for: .touchUpInside)
         self.navigationItem.rightBarButtonItem = profileBarButton.load()
@@ -84,4 +99,16 @@ class CustomTabBarViewController: ButtonBarPagerTabStripViewController, ViewMode
         self.viewModel.steps.accept(CoatCodeStep.profileIsRequired(uesrId: 1234)) // 사용자 userId
     }
     
+    func setSearchBar() {
+        let searchController = UISearchController(searchResultsController: nil)
+        self.navigationItem.titleView = searchController.searchBar
+        self.navigationItem.searchController?.hidesNavigationBarDuringPresentation = false
+        
+        searchController.searchBar.rx.tapGesture()
+            .when(.recognized)
+            .subscribe(onNext: { [weak self] _ in
+                self?.viewModel.steps.accept(CoatCodeStep.searchIsRequired)
+                searchController.searchBar.endEditing(true)
+            }).disposed(by: disposeBag)
+    }
 }

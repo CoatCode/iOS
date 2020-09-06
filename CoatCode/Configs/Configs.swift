a213f140bf672bbfaf35d076bbdca7006addecbf
KangMinseok <qbq5000@gmail.com>
2020-09-06 15:10:25 +0900

    [Add] SearchFlow & Configs 추가

---
+++ CoatCode/Configs/Configs.swift
@@ -0,0 +1,34 @@
+//
+//  Configs.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/06.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+struct Configs {
+    
+    struct App {
+        static let bundleIdentifier = ""
+    }
+    
+    struct Network {
+        static let baseURL = ""
+    }
+    
+    struct BaseDimensions {
+        
+    }
+    
+    struct BaseColor {
+        static let signatureColor = UIColor(red: 0.20, green: 0.20, blue: 0.20, alpha: 1.00)
+        static let navBarTintColor = UIColor(red: 0.99, green: 0.99, blue: 0.99, alpha: 1.00)
+    }
+    
+    struct Path {
+        
+    }
+    
+}
---
+++ CoatCode/Flows/Search/SearchFlow.swift
@@ -0,0 +1,15 @@
+//
+//  SearchFlow.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/06.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import RxFlow
+import RxSwift
+import RxCocoa
+
+class SearchFlow {
+    
+}

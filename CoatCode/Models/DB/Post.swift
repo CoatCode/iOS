c822799d93c2721c599ecfac4b31f0b0fa2fc7f9
KangMinseok <qbq5000@gmail.com>
2020-09-07 20:58:45 +0900

    [Add] 피드 게시물 모델 추가

---
+++ CoatCode/Models/DB/Post.swift
@@ -0,0 +1,15 @@
+//
+//  Post.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/07.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+struct Post: Codable {
+    
+    
+    
+}

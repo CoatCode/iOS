44453b41e3041a31a37c608ea45b0b4784c779d7
KangMinseok <qbq5000@gmail.com>
2020-09-13 21:01:03 +0900

    [Add] 피드 콜렉션 뷰 셀 추가

---
+++ CoatCode/Modules/MainTabBar/Feed/Cell/FeedCollectionViewCell.swift
@@ -0,0 +1,18 @@
+//
+//  FeedCollectionViewCell.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/13.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class FeedCollectionViewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}

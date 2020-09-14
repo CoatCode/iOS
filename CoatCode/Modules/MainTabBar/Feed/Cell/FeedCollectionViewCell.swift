ba507e1e32a47b4a132cf478cbe014685e2afe29
KangMinseok <qbq5000@gmail.com>
2020-09-14 22:25:33 +0900

    [Modify] 피드 CollectionView 구성

--- CoatCode/Modules/MainTabBar/Feed/Cell/FeedCollectionViewCell.swift
+++ CoatCode/Modules/MainTabBar/Feed/Cell/FeedCollectionViewCell.swift
@@ -10,6 +10,19 @@ import UIKit
 
 class FeedCollectionViewCell: UICollectionViewCell {
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var moreButton: UIButton!
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var likeImageView: UIImageView!
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var commentImageView: UIImageView!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    @IBOutlet weak var viewCountLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code

b0383eefb97b0d35f1d7b156570e95c476fe0474
KangMinseok <qbq5000@gmail.com>
2020-09-22 23:56:45 +0900

    [Modify] 게시물 상세 ViewController CollectionView DataSource 구성

--- CoatCode/Modules/PostDetail/PostDetailViewController.swift
+++ CoatCode/Modules/PostDetail/PostDetailViewController.swift
@@ -8,14 +8,42 @@
 
 import UIKit
 import Reusable
+import RxDataSources
+import RxSwift
+import RxCocoa
 
 class PostDetailViewController: BaseViewController, StoryboardSceneBased {
 
     static let sceneStoryboard = UIStoryboard(name: "PostDetail", bundle: nil)
     
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+    }
+    
+    override func bindViewModel() {
+        super.bindViewModel()
+        
+        let input = PostDetailViewModel.Input()
+        let ouput = viewModel.transform(input: input)
+        
+        let dataSource = RxTableViewSectionedReloadDataSource<PostDetailSection>(configureCell: { dataSource, tableView, indexPath, item in
+            switch item {
+            case.contentItem(let viewModel):
+                let cell = (tableView.dequeueReusableCell(withIdentifier: "", for: indexPath) as? PostDetailCell)!
+                cell.bind(to: viewModel)
+                return cell
+            case .commentItem(let viewModel):
+                let cell (tableView.dequeueReusableCell(withIdentifier: "", for: indexPath) as? CommentCell)!
+                cell.bind(to: viewModel)
+                return cell
+            }
+            
+        })
+        
+        
     }
     
     

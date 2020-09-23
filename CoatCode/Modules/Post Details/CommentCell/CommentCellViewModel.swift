35a225a59ae64101ae8858db6700ac0f0ed2add1
KangMinseok <qbq5000@gmail.com>
2020-09-23 23:12:46 +0900

    [Add] 댓글 CellViewModel 추가

---
+++ CoatCode/Modules/Post Details/CommentCell/CommentCellViewModel.swift
@@ -0,0 +1,17 @@
+//
+//  CommentCellViewModel.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/09/23.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+
+class CommentCellViewModel {
+    
+    
+    
+}

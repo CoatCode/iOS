ec9bb736452991a155fb21dddd453adc04ab7ba7
KangMinseok <qbq5000@gmail.com>
2020-09-25 20:33:24 +0900

    [Modify] 게시물 상세보기 CollectionViewCell 구성

--- CoatCode/Modules/Post Details/PostDetailCell/PostDetailCell.swift
+++ CoatCode/Modules/Post Details/PostDetailCell/PostDetailCell.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class PostDetailCell: UICollectionViewCell {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var likeButton: UIButton!
+    @IBOutlet weak var shareButton: UIButton!
+    
+    @IBOutlet weak var contentLabel: UILabel!
+    @IBOutlet weak var tagLabel: UILabel!
+    
+    @IBOutlet weak var likeCountLabel: UILabel!
+    @IBOutlet weak var viewCountLabel: UILabel!
+    @IBOutlet weak var commentCountLabel: UILabel!
+    
+    @IBOutlet weak var createTimeLabel: UILabel!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+    }
+    
+    func bind(to viewModel: PostDetailCellViewModel) {
+        
     }
 
 }

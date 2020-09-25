4db40498dd8df3038e46413586041d81e34070ba
KangMinseok <qbq5000@gmail.com>
2020-09-25 22:24:51 +0900

    [Modify] PostDetailViewModel 수정 + 변수 추가 + 생성자 추가

--- CoatCode/Modules/Post Details/PostDetailCell/PostDetailCellViewModel.swift
+++ CoatCode/Modules/Post Details/PostDetailCell/PostDetailCellViewModel.swift
@@ -12,18 +12,29 @@ import RxCocoa
 
 class PostDetailCellViewModel {
     
-    let contentImageUrls = BehaviorRelay<[String]?>(value: nil)
+    let contentImageUrls = BehaviorRelay<[URL]?>(value: nil)
+    let title = BehaviorRelay<String?>(value: nil)
     let content = BehaviorRelay<String?>(value: nil)
     let likeCount = BehaviorRelay<Int?>(value: nil)
     let commentCount = BehaviorRelay<Int?>(value: nil)
     let viewCount = BehaviorRelay<Int?>(value: nil)
     let isLiked = BehaviorRelay<Bool?>(value: nil)
     let tag = BehaviorRelay<String?>(value: nil)
+    let createdTime = BehaviorRelay<Date?>(value: nil)
     
     let post: Post
     
     init(with post: Post) {
         self.post = post
+        
+        self.contentImageUrls.accept(post.imageURLs)
+        self.title.accept(post.title)
+        self.content.accept(post.content)
+        self.likeCount.accept(post.likeCount)
+        self.commentCount.accept(post.commentCount)
+        self.viewCount.accept(post.viewCount)
+        self.isLiked.accept(post.isLiked)
+        self.tag.accept(post.tag)
+        self.createdTime.accept(post.createdAt)
     }
-    
 }

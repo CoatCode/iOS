fb65341ac082752bc971d4be7eefbdf9dc0f7b1f
KangMinseok <qbq5000@gmail.com>
2020-09-28 17:38:09 +0900

    [Modify] 게시물 상세보기 댓글 표시 작업

--- CoatCode/Modules/Post Details/PostDetailViewModel.swift
+++ CoatCode/Modules/Post Details/PostDetailViewModel.swift
@@ -12,6 +12,7 @@ import RxCocoa
 class PostDetailViewModel: BaseViewModel {
     
     let post: BehaviorRelay<Post>
+    let comment = BehaviorRelay<[Comment]>(value: [Comment(id: -1, owner: User(), content: "", createdAt: Date())])
     
     init(with post: Post) {
         self.post = BehaviorRelay(value: post)
@@ -29,12 +30,33 @@ class PostDetailViewModel: BaseViewModel {
 
 extension PostDetailViewModel {
     func transform(input: Input) -> Output {
+        
         let items = post.map { post -> [PostDetailSection] in
             var items: [PostDetailSectionItem] = []
+            
+            // Post
             let postDetailCellViewModel = PostDetailCellViewModel(with: self.post.value)
             items.append(PostDetailSectionItem.postDetailItem(viewModel: postDetailCellViewModel))
             
-            return [PostDetailSection.post(title: "", items: items)]
+            // Comment
+            self.services.feedComments(postId: post.id)
+                .trackActivity(self.loading)
+                .subscribe(onNext: { [weak self] comment in
+                    self?.comment.accept(comment)
+                }).disposed(by: self.disposeBag)
+            
+//            if let comments = self.comment.value.map({ CommentCellViewModel(with: $0) }) {
+//                comments.forEach({ (cellViewModel) in
+//                    items.append(PostDetailSectionItem.commentItem(viewModel: cellViewModel))
+//                })
+//            }
+            
+            let comments = self.comment.value.map { CommentCellViewModel(with: $0) }
+            comments.forEach { (cellViewModel) in
+                items.append(PostDetailSectionItem.commentItem(viewModel: cellViewModel))
+            }
+            
+            return [PostDetailSection.post(title: "", items: items)] 
         }
         
         return Output(items: items)

f3fbf47f1bcb8f4506f5cf36e31782a2fd7f1235
KangMinseok <qbq5000@gmail.com>
2020-10-07 00:08:35 +0900

    [Modify] FeedCellViewModel 좋아요 기능 관련 작업

--- CoatCode/Modules/MainTabBar/Feed/Cell/FeedCellViewModel.swift
+++ CoatCode/Modules/MainTabBar/Feed/Cell/FeedCellViewModel.swift
@@ -14,24 +14,28 @@ class FeedCellViewModel {
     let profileName = BehaviorRelay<String?>(value: nil)
     let profileImageUrl = BehaviorRelay<String?>(value: nil)
     let contentImageUrl = BehaviorRelay<URL?>(value: nil)
-    let content = BehaviorRelay<String?>(value: nil)
+    let title = BehaviorRelay<String?>(value: nil)
     let likeCount = BehaviorRelay<Int?>(value: nil)
     let commentCount = BehaviorRelay<Int?>(value: nil)
     let viewCount = BehaviorRelay<Int?>(value: nil)
     let isLiked = BehaviorRelay<Bool?>(value: nil)
+    let createdTime = BehaviorRelay<Date?>(value: nil)
     
-    let post: Post
+    let disposeBag = DisposeBag()
+    var services: CoatCodeService!
+    var post: Post
     
     init(with post: Post) {
         self.post = post
         
-        profileName.accept(post.owner.username)
-        profileImageUrl.accept(post.owner.profile)
-        contentImageUrl.accept(post.imageURLs?.first)
-        content.accept(post.content)
-        likeCount.accept(post.likeCount)
-        commentCount.accept(post.commentCount)
-        viewCount.accept(post.viewCount)
+        self.profileName.accept(post.owner.username)
+        self.profileImageUrl.accept(post.owner.profile)
+        self.contentImageUrl.accept(post.imageURLs?.first)
+        self.title.accept(post.title)
+        self.likeCount.accept(post.likeCount)
+        self.commentCount.accept(post.commentCount)
+        self.viewCount.accept(post.viewCount)
+        self.createdTime.accept(post.createdAt)
         
         // 좋아요 여부
         guard let isLiked = post.likedPeoples?.contains(DatabaseManager.shared.getCurrentUser().id) else { return }
@@ -42,4 +46,34 @@ class FeedCellViewModel {
             self.isLiked.accept(false)
         }
     }
+    
+    func like() {
+        self.isLiked.accept(true)
+        self.likeCount.accept((self.likeCount.value ?? 0) + 1)
+        self.post.likeCount = (self.likeCount.value ?? 0) + 1
+
+        self.services.likePost(postId: post.id)
+            .asObservable()
+            .subscribe(onNext: { _ in
+                
+            }, onError: { _ in
+                
+            }).disposed(by: disposeBag)
+    }
+    
+    func unLike() {
+        self.isLiked.accept(false)
+        self.likeCount.accept((self.likeCount.value ?? 0) - 1)
+        self.post.likeCount = (self.likeCount.value ?? 0) - 1
+        
+        // unlike service
+        self.services.unlikePost(postId: post.id)
+            .asObservable()
+            .subscribe(onNext: { _ in
+                
+            }, onError: { _ in
+                
+            }).disposed(by: disposeBag)
+    }
+    
 }

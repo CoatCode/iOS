21b146f7eddc29a94711c0ec1fa6f5d6b91b4634
KangMinseok <qbq5000@gmail.com>
2020-10-18 23:36:14 +0900

    [Add / Delete] FeedCellVM과 PostDetailCellVM을 PostCellVM로 대체

--- CoatCode/Modules/Post Details/PostDetailCell/PostCellViewModel.swift
+++ CoatCode/Modules/Post Details/PostDetailCell/PostCellViewModel.swift
@@ -1,36 +1,45 @@
 //
-//  PostDetailCellViewModel.swift
+//  PostCellViewModel.swift
 //  CoatCode
 //
-//  Created by 강민석 on 2020/09/23.
+//  Created by 강민석 on 2020/10/18.
 //  Copyright © 2020 MinseokKang. All rights reserved.
 //
 
-import Foundation
 import RxSwift
 import RxCocoa
 
-class PostDetailCellViewModel {
+class PostCellViewModel {
     
-    let contentImageUrls = BehaviorRelay<[URL]?>(value: nil)
+    // writer profile
+    let username = BehaviorRelay<String?>(value: nil)
+    let profileImageUrl = BehaviorRelay<String?>(value: nil)
+    
+    // Post Content
+    let imageUrls = BehaviorRelay<[URL]?>(value: nil)
     let title = BehaviorRelay<String?>(value: nil)
     let content = BehaviorRelay<String?>(value: nil)
+    
     let likeCount = BehaviorRelay<Int?>(value: nil)
     let commentCount = BehaviorRelay<Int?>(value: nil)
     let viewCount = BehaviorRelay<Int?>(value: nil)
-    let isLiked = BehaviorRelay<Bool?>(value: nil)
+    
     let tag = BehaviorRelay<String?>(value: nil)
     let createdTime = BehaviorRelay<Date?>(value: nil)
     
+    let isLiked = BehaviorRelay<Bool?>(value: nil)
+    
     let disposeBag = DisposeBag()
-    var services: CoatCodeService!
+    var services: CoatCodeService
     var post: Post
     
     init(post: Post, services: CoatCodeService) {
         self.post = post
         self.services = services
         
-        self.contentImageUrls.accept(post.imageURLs)
+        self.username.accept(post.owner.username)
+        self.profileImageUrl.accept(post.owner.profile)
+        self.imageUrls.accept(post.imageURLs)
         self.title.accept(post.title)
         self.content.accept(post.content)
         self.likeCount.accept(post.likeCount)
@@ -39,13 +48,12 @@ class PostDetailCellViewModel {
         self.tag.accept(post.tag)
         self.createdTime.accept(post.createdAt)
         
-        self.services.isLikedPost(postId: post.id)
-            .asObservable()
-            .subscribe(onNext: { [weak self] _ in
-                self?.isLiked.accept(true)
-            }, onError: { [weak self] _ in
-                self?.isLiked.accept(false)
-            }).disposed(by: disposeBag)
+        // 좋아요 여부
+        if let isLiked = post.likedPeople?.contains(DatabaseManager.shared.getCurrentUser().id) {
+            self.isLiked.accept(isLiked)
+        } else {
+            self.isLiked.accept(false)
+        }
     }
     
     func like() {
@@ -55,22 +63,32 @@ class PostDetailCellViewModel {
                 guard let self = self else { return }
                 self.isLiked.accept(true)
                 self.likeCount.accept((self.likeCount.value ?? 0) + 1)
-                self.post.likeCount = (self.likeCount.value ?? 0) + 1
-            }, onError: { error in
-                print(error)
+//                self.post.likeCount = (self.likeCount.value ?? 0) + 1
+            }, onError: { [weak self] error in
+//                self?.unLike()
             }).disposed(by: disposeBag)
     }
     
     func unLike() {
         self.services.unlikePost(postId: post.id)
             .asObservable()
-            .subscribe(onNext: { [weak self] _ in
+            .subscribe(onNext: { [weak self] in
                 guard let self = self else { return }
                 self.isLiked.accept(false)
                 self.likeCount.accept((self.likeCount.value ?? 0) - 1)
-                self.post.likeCount = (self.likeCount.value ?? 0) - 1
-            }, onError: { error in
-                print(error)
+//                self.post.likeCount = (self.likeCount.value ?? 0) - 1
+            }, onError: { [weak self] error in
+//                self?.like()
+            }).disposed(by: disposeBag)
+    }
+    
+    func checkLiking() {
+        self.services.isLikedPost(postId: post.id)
+            .asObservable()
+            .subscribe(onNext: { [weak self] _ in
+                self?.isLiked.accept(true)
+            }, onError: { [weak self] _ in
+                self?.isLiked.accept(false)
             }).disposed(by: disposeBag)
     }
 }

d1168df439820b12e1b0051a2a96dd4da3062802
KangMinseok <qbq5000@gmail.com>
2020-10-20 10:15:49 +0900

    [Add] CommentPreviewCell 추가 + 댓글 미리보기

---
+++ CoatCode/Modules/MainTabBar/Feed/CommentPreviewCell/CommentPreviewCell.swift
@@ -0,0 +1,18 @@
+//
+//  CommentPreviewCell.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/10/20.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+
+class CommentPreviewCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ CoatCode/Modules/MainTabBar/Feed/CommentPreviewCell/CommentPreviewCellViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  CommentPreviewCellViewModel.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/10/20.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import Foundation
+
+class CommentPreviewCellViewModel {
+    
+}

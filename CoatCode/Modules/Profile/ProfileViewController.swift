b6ea3d7e8779beb66f9631d4ec17de03b84cef98
KangMinseok <qbq5000@gmail.com>
2020-11-06 23:48:51 +0900

    [ADD] 프로필(상세) 화면 코드 추가

--- CoatCode/Modules/Profile/ProfileViewController.swift
+++ CoatCode/Modules/Profile/ProfileViewController.swift
@@ -15,9 +15,86 @@ class ProfileViewController: BaseViewController, StoryboardSceneBased {
 
     static let sceneStoryboard = R.storyboard.profile()
 
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+
+    @IBOutlet weak var followerCountLabel: UILabel!
+    @IBOutlet weak var followingCountLabel: UILabel!
+    
+    @IBOutlet weak var followerCountView: UIStackView!
+    @IBOutlet weak var followingCountView: UIStackView!
+
+    @IBOutlet weak var postCountLabel: UILabel!
+    @IBOutlet weak var productCountLabel: UILabel!
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        self.profileImageView.roundCorners(.allCorners, radius: 0.5 * profileImageView.frame.width)
+        self.collectionView.register(R.nib.postPreviewCell)
+        self.collectionView.rx.setDelegate(self).disposed(by: disposeBag)
     }
 
+    override func bindViewModel() {
+        super.bindViewModel()
+
+        guard let viewModel = self.viewModel as? ProfileViewModel else { fatalError("ViewModel Casting Falid!") }
+        
+        let followButton = UIBarButtonItem(image: R.image.bt_Follow(), style: .plain, target: nil, action: nil)
+        self.navigationItem.rightBarButtonItem = followButton
+        
+        let followerSelection = followerCountView.rx.tapGesture().map { _ in }.asDriver(onErrorJustReturn: ())
+        let followingSelection = followingCountView.rx.tapGesture().map { _ in }.asDriver(onErrorJustReturn: ())
+        
+        let input = ProfileViewModel.Input(followSelection: followButton.rx.tap.asObservable(),
+                                           followerSelection: followerSelection,
+                                           followingSelection: followingSelection,
+                                           selection: collectionView.rx.modelSelected(PostPreviewCellViewModel.self).asDriver())
+        let output = viewModel.transform(input: input)
+        
+        output.postItems.asDriver(onErrorJustReturn: [])
+            .drive(collectionView.rx.items(cellIdentifier: R.reuseIdentifier.postPreviewCell.identifier, cellType: PostPreviewCell.self)) { collectionView, viewModel, cell in
+                cell.bind(to: viewModel)
+            }.disposed(by: disposeBag)
+        
+        output.profileUrl.drive(onNext: { [weak self] (imageUrl) in
+            self?.profileImageView.kf.setImage(with: imageUrl)
+        }).disposed(by: disposeBag)
+        
+        output.username.drive(nameLabel.rx.text).disposed(by: disposeBag)
+        output.description.drive(descriptionLabel.rx.text).disposed(by: disposeBag)
+        output.followerCount.drive(followerCountLabel.rx.text).disposed(by: disposeBag)
+        output.followingCount.drive(followingCountLabel.rx.text).disposed(by: disposeBag)
+        output.postCount.drive(postCountLabel.rx.text).disposed(by: disposeBag)
+        output.productCount.drive(productCountLabel.rx.text).disposed(by: disposeBag)
+        
+        output.following.drive(onNext: { (isFollow) in
+            followButton.image = isFollow == true ? R.image.bt_UnFollow() : R.image.bt_Follow()
+        }).disposed(by: disposeBag)
+        
+        output.hidesFollowButton.drive(onNext: { [weak self] (isHide) in
+            if isHide {
+                self?.navigationItem.rightBarButtonItem = nil
+            }
+        }).disposed(by: disposeBag)
+    }
+}
+
+extension ProfileViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        guard let layout = collectionViewLayout as? UICollectionViewFlowLayout else { return CGSize() }
+        let width = (collectionView.bounds.width - layout.minimumInteritemSpacing - layout.sectionInset.left - layout.sectionInset.right) / 3
+        
+        return CGSize(width: width - 3, height: width - 3)
+    }
+    
+    // 지정된 섹션의 셀 사이의 최소간격을 반환하는 메서드.
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
+    }
 }
--- CoatCode/Modules/Profile/ProfileViewModel.swift
+++ CoatCode/Modules/Profile/ProfileViewModel.swift
@@ -11,10 +11,111 @@ import RxCocoa
 
 class ProfileViewModel: BaseViewModel {
 
-    let userId: Int
+    let user: BehaviorRelay<User>
 
-    init(userId: Int) {
-        self.userId = userId
+    init(user: User) {
+        self.user =  BehaviorRelay(value: user)
     }
-
+    
+    struct Input {
+        let followSelection: Observable<Void>
+        let followerSelection: Driver<Void>
+        let followingSelection: Driver<Void>
+        let selection: Driver<PostPreviewCellViewModel>
+    }
+    
+    struct Output {
+        let postItems: Observable<[PostPreviewCellViewModel]>
+        
+        let profileUrl: Driver<URL?>
+        let username: Driver<String>
+        let description: Driver<String>
+        let followerCount: Driver<String>
+        let followingCount: Driver<String>
+        let postCount: Driver<String>
+        let productCount: Driver<String>
+        
+        let following: Driver<Bool>
+        let hidesFollowButton: Driver<Bool>
+    }
+    
+    func transform(input: Input) -> Output {
+        let following = BehaviorRelay(value: false)
+        
+        let followed = input.followSelection.flatMapLatest { [weak self] () -> Observable<Void> in
+            guard let self = self else { return Observable.just(()) }
+            let userId = self.user.value.id
+            let request = following.value == true ? self.services.unFollowUser(userId: userId) : self.services.followUser(userId: userId) 
+            return request
+                .trackActivity(self.loading)
+        }
+        
+        input.followerSelection
+            .drive(onNext: { [weak self] in
+//                self?.steps.accept(<#T##event: Step##Step#>)
+            }).disposed(by: disposeBag)
+        
+        input.followingSelection
+            .drive(onNext: { [weak self] in
+//                self?.steps.accept(<#T##event: Step##Step#>)
+            }).disposed(by: disposeBag)
+        
+        input.selection
+            .drive(onNext: { [weak self] cellViewModel in
+                guard let self = self else { return }
+                let post = cellViewModel.post
+                let postCellViewModel = PostCellViewModel(post: post, services: self.services)
+                self.steps.accept(CoatCodeStep.postDetailIsRequired(cellViewModel: postCellViewModel))
+            }).disposed(by: disposeBag)
+        
+        let postItems = self.user.flatMapLatest { [weak self] (user) -> Observable<[PostPreviewCellViewModel]> in
+            guard let self = self else { return Observable.just([]) }
+            return self.services.userPosts(userId: user.id)
+                .trackActivity(self.loading)
+                .map { $0.map { PostPreviewCellViewModel(post: $0) } }
+        }.share()
+        
+        let refreshFollowing = Observable.of(user.map { _ in }.asObservable(), followed).merge()
+        refreshFollowing.flatMapLatest { [weak self] () -> Observable<RxSwift.Event<Void>> in
+            guard let self = self else { return Observable.just(RxSwift.Event.next(())) }
+            let userId = self.user.value.id
+            return self.services.isFollowUser(userId: userId)
+                .trackActivity(self.loading)
+                .materialize()
+        }.subscribe(onNext: { (event) in
+            switch event {
+            case .next:
+                following.accept(true)
+            case .error:
+                following.accept(false)
+            case .completed:
+                break
+            }
+        }).disposed(by: disposeBag)
+        
+        let hidesFollowButton = user.map({ (user) -> Bool in
+            let currentUser = DatabaseManager.shared.getCurrentUser()
+            return user.id == currentUser.id
+        }).asDriver(onErrorJustReturn: false)
+        
+        let profileUrl = user.map { URL(string: $0.image!) }.asDriver(onErrorJustReturn: URL(fileURLWithPath: ""))
+        let username = user.map { $0.username }.asDriver(onErrorJustReturn: "")
+        let description = user.map { $0.username  }.asDriver(onErrorJustReturn: "") // user 모델에 추가해야함
+        let folllowerCount = user.map { "\($0.followers)" }.asDriver(onErrorJustReturn: "0")
+        let followingCount = user.map { "\($0.following)" }.asDriver(onErrorJustReturn: "0")
+                                                                                        
+        let postCount = postItems.map { "\($0.count)" }.asDriver(onErrorJustReturn: "0")
+        let productCount = postItems.map { "\($0.count)" }.asDriver(onErrorJustReturn: "0") // 상품 받아올 경우에 추가해야함
+        
+        return Output(postItems: postItems,
+                      profileUrl: profileUrl,
+                      username: username,
+                      description: description,
+                      followerCount: folllowerCount,
+                      followingCount: followingCount,
+                      postCount: postCount,
+                      productCount: productCount,
+                      following: following.asDriver(),
+                      hidesFollowButton: hidesFollowButton)
+    }
 }

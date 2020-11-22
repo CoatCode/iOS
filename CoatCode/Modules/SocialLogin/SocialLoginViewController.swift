ce7d0f18786698348fd1526628937e51b5d8dd88
KangMinseok <qbq5000@gmail.com>
2020-11-22 22:10:43 +0900

    [REFACTOR] 소셜로그인 스토리보드 분리

---
+++ CoatCode/Modules/SocialLogin/SocialLoginViewController.swift
@@ -0,0 +1,38 @@
+//
+//  SocialLoginViewController.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/08/10.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import Reusable
+import RxSwift
+import RxCocoa
+
+class SocialLoginViewController: BaseViewController, StoryboardSceneBased {
+
+    // MARK: - Properties
+    static let sceneStoryboard = R.storyboard.socialLogin()
+
+    @IBOutlet weak var dismissButton: UIButton!
+
+    // View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - BindViewModel
+    override func bindViewModel() {
+        super.bindViewModel()
+
+        guard let viewModel = self.viewModel as? SocialLoginViewModel else { fatalError("ViewModel Casting Falid!") }
+
+        dismissButton.rx.tap
+            .subscribe(onNext: {
+                viewModel.dismiss()
+            }).disposed(by: disposeBag)
+    }
+}

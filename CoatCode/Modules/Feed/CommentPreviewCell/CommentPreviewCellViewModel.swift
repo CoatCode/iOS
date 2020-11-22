07a50c8617ac11b8baa366eb445017b8d8cd5ae6
KangMinseok <qbq5000@gmail.com>
2020-11-22 22:26:49 +0900

    [REFACTOR] 댓글 미리보기 셀 리팩토링

---
+++ CoatCode/Modules/Feed/CommentPreviewCell/CommentPreviewCellViewModel.swift
@@ -0,0 +1,28 @@
+//
+//  CommentPreviewCellViewModel.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/10/20.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import RxSwift
+import RxCocoa
+
+class CommentPreviewCellViewModel {
+
+    let writerName = BehaviorRelay<String?>(value: nil)
+    let writerImageUrl = BehaviorRelay<URL?>(value: nil)
+    let content = BehaviorRelay<String?>(value: nil)
+
+    let comment: Comment
+
+    init(comment: Comment) {
+        self.comment = comment
+
+        self.writerName.accept(comment.owner?.username)
+        self.writerImageUrl.accept(URL(string: comment.owner?.image ?? ""))
+        self.content.accept(comment.content)
+    }
+
+}

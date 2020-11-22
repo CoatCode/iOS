b8f91525b452f16e5fb9f1f810c28f223a266747
KangMinseok <qbq5000@gmail.com>
2020-11-22 22:10:23 +0900

    [REFACTOR] 로그인 스토리보드 분리

---
+++ CoatCode/Modules/SignIn/SignInViewController.swift
@@ -0,0 +1,52 @@
+//
+//  LoginViewController.swift
+//  CoatCode
+//
+//  Created by 강민석 on 2020/07/30.
+//  Copyright © 2020 MinseokKang. All rights reserved.
+//
+
+import UIKit
+import Reusable
+import RxSwift
+import RxCocoa
+
+class SignInViewController: BaseViewController, StoryboardSceneBased {
+
+    // MARK: - Properties
+    static let sceneStoryboard = R.storyboard.signIn()
+
+    @IBOutlet weak var emailField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var forgetPwButton: UIButton!
+    @IBOutlet weak var signInButton: UIButton!
+
+    // MARK: - View life cycle
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        backBarButton()
+    }
+
+    // MARK: - BindViewModel
+    override func bindViewModel() {
+        super.bindViewModel()
+
+        guard let viewModel = viewModel as? SignInViewModel else { fatalError("ViewModel Casting Falid!") }
+
+        let input = SignInViewModel.Input(signInTrigger: signInButton.rx.tap.asDriver())
+        let output = viewModel.transform(input: input)
+
+        emailField.rx.text.orEmpty
+            .bind(to: viewModel.email)
+            .disposed(by: disposeBag)
+
+        passwordField.rx.text.orEmpty
+            .bind(to: viewModel.password)
+            .disposed(by: disposeBag)
+
+        output.loginButtonEnabled
+            .drive(signInButton.rx.isEnabled)
+            .disposed(by: disposeBag)
+    }
+}

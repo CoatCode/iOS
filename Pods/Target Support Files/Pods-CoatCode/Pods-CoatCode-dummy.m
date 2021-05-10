e1cdd4286d7d319a21b84921c9168534fef74e0a
KangMinseok <qbq5000@gmail.com>
2021-05-10 09:50:31 +0900

    🙈 gitignore 수정
    + Pod 파일 제외

--- Pods/Target Support Files/Pods-CoatCode/Pods-CoatCode-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoatCode : NSObject
-@end
-@implementation PodsDummy_Pods_CoatCode
-@end
--- Pods/Target Support Files/RxTags/RxTags-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_RxTags : NSObject
-@end
-@implementation PodsDummy_RxTags
-@end
